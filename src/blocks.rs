de6ef50a61f90f4f4b4aa3c3ac1b789cb822e789
Pontus Laos <pontus.laos@volvocars.com>
2021-11-25 14:59:27 +0100

    Overhaul custom blocks
    Now only use two blocks. Define a function for running a command more
    ergonomically.

--- src/blocks.rs
+++ src/blocks.rs
@@ -1,26 +1,43 @@
 use std::process::Command;
+use std::fs;
 
-use crate::BlockFn::{self, External, Internal};
+use crate::BlockFn::{self, Internal};
+
+fn run_cmd(cmd: &str, args: &[&str], envs: &[(&str, &str)]) -> String {
+    let mut command = Command::new(cmd);
+    command.args(args);
+
+    for (k, v) in envs {
+        command.env(k, v);
+    }
+
+    String::from_utf8(command.output().unwrap().stdout)
+        .unwrap()
+        .trim_end()
+        .into()
+}
 
 pub fn date() -> String {
-    let date = Command::new("date").output().unwrap();
-    String::from_utf8(date.stdout).unwrap()
+    run_cmd("date", &["+%a %b %d %H:%M:%S"], &[("LC_ALL", "en")])
 }
 
-pub fn bat() -> &'static str {
-    "/home/plaos/.local/scripts/statusbar/sb-bat"
-}
+pub fn bat() -> String {
+    let cap: String = fs::read_to_string("/sys/class/power_supply/BAT0/capacity").unwrap().trim_end().into();
+    let status: String = fs::read_to_string("/sys/class/power_supply/BAT0/status").unwrap().trim_end().into();
 
-pub fn cpu() -> &'static str {
-    "/home/plaos/.local/scripts/statusbar/sb-cpu"
+    // let cap = run_cmd("cat", &["/sys/class/power_supply/BAT0/capacity"], &[]);
+    // let status = run_cmd("cat", &["/sys/class/power_supply/BAT0/status"], &[]);
+
+    let status = match status.as_ref() {
+        "Charging" => "+",
+        "Discharging" | "Full" => "",
+        _ => "?",
+    };
+    format!("[{}{}%]", status, cap)
 }
 
 pub const DELIM: &str = "  ";
 pub const BLOCKS: &[(u32, BlockFn)] = &[
-    (1800, External(|| "/home/plaos/.local/scripts/statusbar/sb-updates")),
-    (5   , External(cpu)),
-    (3   , External(|| "/home/plaos/.local/scripts/statusbar/sb-mem")),
-    (300 , External(|| "/home/plaos/.local/scripts/statusbar/sb-disk")),
-    (30  , External(bat)),
-    (1   , Internal(date)),
+    (30, Internal(bat)),
+    (1, Internal(date)),
 ];
