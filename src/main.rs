52ef06517345ddff0c575477805a0959fa08828c
Pontus Laos <laos.pontus@gmail.com>
2021-12-01 18:24:45 +0100

    Better error handling
    Split the main() function and catch any error it produces - print the
    error and exit with 1. Pretty standard.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 mod config;
 
 use std::process::Command;
-use std::sync::mpsc::channel;
+use std::sync::mpsc::{RecvError, channel};
 use std::thread;
 use std::time::Duration;
 
@@ -10,11 +10,24 @@ use crate::config::{BLOCKS, DELIM};
 /// The signature of a block. Anything with this signature
 /// can be used as a block.
 pub type Procedure = fn() -> String;
+
 /// A Block is the combination of an interval and a procedure,
 /// evaluating the <procedure> each <interval> seconds.
 pub type Block = (Duration, Procedure);
 
+/// Internal representation of something failable.
+type Result<T> = std::result::Result<T, RecvError>;
+
 fn main() {
+    // If the `run` function errors, print the error and exit.
+    if let Err(e) = run() {
+        eprintln!("{}", e.to_string());
+        std::process::exit(1);
+    }
+}
+
+/// Spawn each block in its own thread, and wait for them to update.
+fn run() -> Result<()> {
     // Evaluate all blocks once on startup
     let mut results: Vec<String> = BLOCKS.iter().map(|(_, block)| block()).collect();
 
@@ -37,7 +50,7 @@ fn main() {
 
     loop {
         // Wait for any block to be updated
-        let (id, msg) = rx.recv().unwrap();
+        let (id, msg) = rx.recv()?;
         results[id] = msg;
 
         // The "statusbar" is the combination of all blocks, with
